61f419ebe6050f7cb5b171043ae6f79c398db5b4
Yevhen Maslov <yevheniimaslov@gmail.com>
2024-12-09 20:07:30 +0200

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/goit-react-hw-01/",
+  base: "/",
   plugins: [react()],
   build: {
     sourcemap: true,
