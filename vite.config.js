842024e95646588a724bcbbe02c2c0e9d2e575ed
Yevhen Maslov <yevheniimaslov@gmail.com>
2024-12-09 18:36:40 +0200

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   }
-// });
-
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-//   base: '/goit-react-hw-01/',
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
